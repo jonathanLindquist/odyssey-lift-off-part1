256dda0490cac40025acc16ffaec8e6fad0db28e
Jonathan Lindquist <26193336+jonathanLindquist@users.noreply.github.com>
2023-10-25 17:58:30 -0500

    bits

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+
+const TRACKS = gql`
+    query TestQuery {
+        tracksForHome {
+            id
+            title
+            length
+            thumbnail
+            modulesCount
+            author {
+                id
+                name
+                photo
+            }
+        }
+    }
+`
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error.message}`;
+
+  return <Layout grid>{JSON.stringify(data)}</Layout>;
 };
 
 export default Tracks;
